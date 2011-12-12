306af7741e4ee1af64b32f5f500f8111659ea85a
Samuel-Tingdao <phtsao+samuel-tingdao@gmail.com>
2011-12-12 13:37:12 +0800

    ad dalll

---
+++ tcc/tcc/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Samuel
+ * Date: 2011/12/10
+ * Time: 22:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tcc
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			
+			
+			
+			Console.WriteLine("hellooo");
+		}
+	}
+}
